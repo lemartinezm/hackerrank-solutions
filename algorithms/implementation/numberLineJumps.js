8a50bd9ce31d8911f6c65729d82d36f080cd2b69
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-10-23 21:37:54 -0500

    algorithms implementation: refactor number line jumps

--- algorithms/implementation/numberLineJumps.js
+++ algorithms/implementation/numberLineJumps.js
@@ -1,17 +1,11 @@
 function kangaroo(x1, v1, x2, v2) {
+  if (v2 > v1) [x1, x2, v1, v2] = [x2, x1, v2, v1]
   if (v1 > v2) {
     while (x1 < x2) {
       x1 += v1
       x2 += v2
       if (x1 === x2) return 'YES'
     }
-  }
-  if (v2 > v1) {
-    while (x2 < x1) {
-      x1 += v1
-      x2 += v2
-      if (x1 === x2) return 'YES'
-    }
-  }
+  } 
   return 'NO'
 }
